760bc8ffddb29a3ea43ac7ac3349217ada1d97d5
João Paulo <joaopauloxavier9@gmail.com>
2026-06-22 19:43:22 -0300

    Corrige a opacidade da sombra no card da página inicial

--- lib/src/ui/pages/home.dart
+++ lib/src/ui/pages/home.dart
@@ -101,7 +101,7 @@ class Home extends StatelessWidget {
                 borderRadius: BorderRadius.circular(12),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
+                    color: Colors.black.withValues(alpha: 0.1),
                     blurRadius: 10,
                     offset: Offset(0, 5),
                   ),
