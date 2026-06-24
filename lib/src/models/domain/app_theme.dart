d42afe421414f3413758d4f5718b9e2165c833a8
João Paulo <joaopauloxavier9@gmail.com>
2026-06-24 11:38:02 -0300

    Adição de temas de contexto

---
+++ lib/src/models/domain/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  final Color primaryColor;
+  final Color secondaryColor;
+  final Color backgroundColor;
+  final Color textColor;
+
+  AppTheme({
+    required this.primaryColor,
+    required this.secondaryColor,
+    required this.backgroundColor,
+    required this.textColor,
+  });
+}
---
+++ lib/src/styles/themes/devotional_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DevotionalColors{
+  static const primary = Color(0xFF7DA56B);
+
+  static const light = Color(0xFFEAF4E6);
+
+  static const surface = Color(0xFFF8FBF6);
+
+  static const dark = Color(0xFF547044);
+
+  static const accent = Color(0xFF96C07D);
+}
